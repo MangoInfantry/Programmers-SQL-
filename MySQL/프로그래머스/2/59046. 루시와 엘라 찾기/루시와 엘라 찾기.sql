1c68122309985b5b62bee487b1d79e76c3bc98f7
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-11-02 22:35:00 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ MySQL/프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT animal_id, name, sex_upon_intake
-from animal_ins as ins
-where name in ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty') and sex_upon_intake = 'Spayed Female'
-order by animal_id
+# 출력: Lucy, Ella, Pickle, Rogan, Sabina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 쿼리
+
+select animal_id, name, sex_upon_intake
+from animal_ins
+where name in ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
