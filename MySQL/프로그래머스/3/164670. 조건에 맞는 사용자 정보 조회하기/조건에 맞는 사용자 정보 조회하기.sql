227251d7f4ec01a1cbbdbb0578840f9e17ff66ab
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-11-13 22:54:45 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ MySQL/프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,11 +1,21 @@
-WITH temp AS (
-    SELECT writer_id
-    FROM used_goods_board
-    GROUP BY writer_id
-    HAVING COUNT(*) >= 3
+# 중고 거래 게시물 3건 이상 등록한 사용자의 사용자 id, 닉네임, 전체주소, 전화번호를 조회하는 sql 쿼리
+
+with cte as (
+    select user_id, nickname, city, street_address1, street_address2, tlno, count(user_id) as cnt
+    from used_goods_board as ub
+    join used_goods_user as us
+    on ub.writer_id = us.user_id
+    group by user_id
+), cte2 as (
+    select user_id, 
+           nickname,
+           concat(city,' ',street_address1,' ',street_address2) as '전체주소',
+           concat(left(tlno,3),'-',substring(tlno,4,4),'-',substring(tlno,8,4)) as '전화번호'
+    from cte
+    where cnt >=3
+    order by user_id desc
 )
 
-SELECT u.user_id, u.nickname, CONCAT(u.city, ' ', u.street_address1, ' ', u.street_address2) AS '전체주소', concat(substring(tlno,1,3),'-',substring(tlno,4,4),'-',substring(tlno,8,4)) as '전화번호'
-FROM used_goods_user u
-JOIN temp t ON u.user_id = t.writer_id
+select user_id, nickname, 전체주소, 전화번호
+from cte2
 order by user_id desc
