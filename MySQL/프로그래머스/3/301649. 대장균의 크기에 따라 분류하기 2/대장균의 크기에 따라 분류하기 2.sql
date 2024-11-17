d9ea3456808a62da3347a82800df7f9637289829
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-11-17 13:49:54 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ MySQL/프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,15 +1,15 @@
-with a as(
-select ID,PERCENT_RANK() OVER (ORDER BY size_of_colony DESC) percent
-from ecoli_data
+# 출력: 0~25%를 critical, 26~50%를 high, 51~75를 medium, 76~100프로를 low라 하고, 대장균 id와 분류된 이름을 출력하는 sql 문
+
+with cte as (
+    select id, percent_rank() over (order by size_of_colony desc) as percent
+    from ecoli_data
 )
 
-select e.ID,
-case
-when percent <=0.25 then "CRITICAL"
-when percent <=0.5 then "HIGH"
-when percent <=0.75 then "MEDIUM"
-else "LOW"
-end as COLONY_NAME
-from ecoli_data as e
-join a on e.id=a.id
-order by e.ID
+select id, 
+       case when percent<=0.25 then 'CRITICAL'
+            when percent<=0.5 then 'HIGH'
+            when percent<=0.75 then 'MEDIUM'
+            when percent<=1 then 'LOW' end as colony_name
+from cte
+order by id 
+
