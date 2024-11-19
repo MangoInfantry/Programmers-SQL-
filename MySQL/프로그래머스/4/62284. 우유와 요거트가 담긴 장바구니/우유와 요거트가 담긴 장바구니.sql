9c0adcc114d2de62a0ee04a43dd03453ea09667c
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-11-19 19:01:42 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ MySQL/프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,9 +1,14 @@
-with y as (select cart_id
-           from cart_products
-           where name ='Yogurt')
+# 출력: 우유와 요거트를 동시에 구입한 장바구니 아이디를 조회하는 쿼리
+# cte를 통해서 씨리얼 혹은 요거트를 가진 사람들을 필터링하고, lag를 통해 다음 물품이 뭔지 구한다. 
+# 만약에 lag한것에서 물품이 null이 아니고, name와 lag1이 서로 같지 않다면, 씨리얼과 요거트를 둘다 산 사람.  
 
-select cart_id
-from cart_products
-where cart_id in (select cart_id
-                 from y) and name = 'Milk'
-group by cart_id
+with cte as (
+    select *, lag(name,1) over (partition by cart_id) as lag1
+    from cart_products
+    where name in ('Milk','Yogurt')
+)
+
+select distinct cart_id
+from cte
+where lag1 is not null and name != lag1
+order by cart_id
