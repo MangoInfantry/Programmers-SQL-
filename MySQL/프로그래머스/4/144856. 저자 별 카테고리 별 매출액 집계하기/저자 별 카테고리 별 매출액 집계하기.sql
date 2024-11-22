7e0ea8a02852fc16651704bbc388a2e2a7430886
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-11-22 23:01:28 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ MySQL/프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,13 +1,20 @@
--- 코드를 입력하세요
-with book_author as (SELECT b.book_id, category, a.author_id, author_name, price, published_date , sales_date, sales
-                     from book as b
-                     left join author as a
-                     on b.author_id = a.author_id
-                    join book_sales as bs
-                    on b.book_id = bs.book_id
-                    where month(sales_date)='01')
+# 출력: 2022년 1월 도서 판매 데이터 기준, 저자별, 카테고리 별 매출액
+# 조건: 저자 ID, 저자명, 카테고리, 매출액을 출력하고, 저자 ID 오름차순, 카테고리 내림차순
+# CTE1에서 BOOK_SALES와 BOOK 테이블을 조인하고 SALES_DATE가 1월 달인 것을 필터링
+# CTE1과 AUTHOR 테이블을 AUTHOR_ID를 가지고 조인하고, TOTA_SALES를 열을 생성한 CTE2 생성
+# 문제에서 말한 조건 그대로 출력
 
-select author_id, author_name, category, sum(sales*price) as total_sales
-from book_author as bau
-group by author_id, category
-order by author_id asc, category desc
+with cte1 as (
+    select s.book_id, author_id, category, sales, price, sum(sales*price) as total_sales
+    from book_sales as s
+    join book as b
+    on s.book_id = b.book_id
+    where left(sales_date,7) = '2022-01'
+    group by author_id, category
+)
+
+select a.author_id, author_name, category, total_sales
+from cte1 as c
+join author as a
+on c.author_id = a.author_id
+order by a.author_id asc, category desc
